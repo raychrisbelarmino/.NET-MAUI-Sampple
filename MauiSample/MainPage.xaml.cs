655387a86bc632a5adaa10024bb9332a71e323b8
Chris Ray Belarmino <raychrisbelarmino@gmail.com>
2025-03-17 21:07:40 +0800

    Added sample for animation, resource dictionary and preference

--- MauiSample/MainPage.xaml.cs
+++ MauiSample/MainPage.xaml.cs
@@ -8,14 +8,38 @@ public partial class MainPage : ContentPage
 		NavigationPage.SetHasNavigationBar(this, false);
 	}
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        string firstName = Preferences.Default.Get("fname", "Default value if preference does not exist");
+        Console.WriteLine("Value of first name: " + firstName);
+    }
+
     private void Button_Clicked(object sender, EventArgs e)
     {
-		Navigation.PushAsync(new NavigationPage(new AppShell()));
+        Navigation.PushAsync(new NavigationPage(new AppShell()));
     }
 
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
 		Navigation.PushModalAsync(new Page2());
     }
+
+    private void SetPreference_Clicked(object sender, EventArgs e)
+    {
+        // Set a string, number or boolean value:
+        Preferences.Default.Set("fname", "Chiz");
+    }
+
+    private void Animate_Clicked(object sender, EventArgs e)
+    {
+        //Animation
+        logoImage.RotateTo(90, 5000);
+        logoImage.ScaleTo(0.5, 5000);
+        logoImage.TranslateTo(20, 20, 5000);
+        logoImage.FadeTo(0.5, 5000);
+    }
+
+    
 }
 
